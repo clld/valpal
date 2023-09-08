ddc8125f4a7f5561af5074ed7eef27e05e384fb6
Johannes Englisch <johannes_englisch@eva.mpg.de>
2023-09-08 09:30:32 +0200

    address #3 (for real this time)

--- valpal/assets.py
+++ valpal/assets.py
@@ -6,12 +6,8 @@ import valpal
 
 
 static_path = Path(valpal.__file__).parent / 'static'
-download_path = static_path / 'download'
 
 environment.append_path(
     static_path.as_posix(),
     url='/valpal:static/')
-environment.append_path(
-    download_path.as_posix(),
-    url='/valpal:static/download/')
 environment.load_path = list(reversed(environment.load_path))
